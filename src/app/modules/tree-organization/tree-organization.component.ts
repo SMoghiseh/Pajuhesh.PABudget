13c950a9e05c2f3916ffb74031a925792b4e59c4
M.Jafari <M.Jafari@pajuheshco.com>
2024-05-18 07:03:54 -0700

    fix : complete structure

--- src/app/modules/tree-organization/tree-organization.component.ts
+++ src/app/modules/tree-organization/tree-organization.component.ts
@@ -1,6 +1,8 @@
 import { DOCUMENT } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpService } from '@core/http/http.service';
+import { CompanyTree } from '@shared/models/response.model';
 
 @Component({
   selector: 'PABudget-tree-organization',
@@ -8,62 +10,74 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./tree-organization.component.scss']
 })
 export class TreeOrganizationComponent {
-  data: any = {}
+  companyList: CompanyTree[] = []
 
-  constructor(private router: ActivatedRoute, @Inject(DOCUMENT) document: Document) {
+  constructor(private router: ActivatedRoute, @Inject(DOCUMENT) document: Document,
+    private httpService: HttpService) {
     if (router.snapshot.data['showSideBar'] == false) {
       this.changeStyleOfSideBar();
     }
   }
 
   ngOnInit(): void {
+    this.getAllCompanyList();
 
 
-    this.data = [
-      {
-        title: 'دبیرخانه هیات امنایی سازمان تامین اجتماعی و صندوق',
-        children: [
-          {
-            title: 'صندوق بازنشستگی کشوری',
-            children: [
-              { title: '1سرمایه گذاری آتیه صبا' },
-              { title: '1سرمایه گذاری  صندوقبازنشستگی کشوری' },
-            ]
-          },
-          {
-            title: 'بانک توسعه تعاون',
-            children: [
-              { title: '2سرمایه گذاری  صندوقبازنشستگی کشوری' },
-            ]
-          },
-          {
-            title: 'صندوق حمایت و بازنشستگی آینده ساز ',
-            children: [
-              { title: '3سرمایه گذاری آتیه صبا' },
-              { title: '3گروه مدیریت ارزش سرمایه' },
-              { title: '3گروه مدیریت ارزش سرمایه' }
-            ]
-          },
-          {
-            title: '  صندوق بیمه اجتماعی',
-            children: [
-              { title: '4سرمایه گذاری آتیه صبا' },
-              { title: '4سرمایه گذاری  صندوقبازنشستگی کشوری' },
-              { title: '4گروه مدیریت ارزش سرمایه' }
-            ]
-          },
-          {
-            title: 'صندوق حمایت و بازنشستگی  کارکنان فولاد ',
-            children: [
-              { title: '5سرمایه گذاری آتیه صبا' },
-              { title: '5سرمایه گذاری  صندوقبازنشستگی کشوری' },
-              { title: '5گروه مدیریت ارزش سرمایه' }
-            ]
-          },
-        ]
-      }
-    ]
+    // this.companyList = [
+    //   {
+    //     title: 'دبیرخانه هیات امنایی سازمان تامین اجتماعی و صندوق',
+    //     children: [
+    //       {
+    //         title: 'صندوق بازنشستگی کشوری',
+    //         children: [
+    //           { title: '1سرمایه گذاری آتیه صبا' },
+    //           { title: '1سرمایه گذاری  صندوقبازنشستگی کشوری' },
+    //         ]
+    //       },
+    //       {
+    //         title: 'بانک توسعه تعاون',
+    //         children: [
+    //           { title: '2سرمایه گذاری  صندوقبازنشستگی کشوری' },
+    //         ]
+    //       },
+    //       {
+    //         title: 'صندوق حمایت و بازنشستگی آینده ساز ',
+    //         children: [
+    //           { title: '3سرمایه گذاری آتیه صبا' },
+    //           { title: '3گروه مدیریت ارزش سرمایه' },
+    //           { title: '3گروه مدیریت ارزش سرمایه' }
+    //         ]
+    //       },
+    //       {
+    //         title: '  صندوق بیمه اجتماعی',
+    //         children: [
+    //           { title: '4سرمایه گذاری آتیه صبا' },
+    //           { title: '4سرمایه گذاری  صندوقبازنشستگی کشوری' },
+    //           { title: '4گروه مدیریت ارزش سرمایه' }
+    //         ]
+    //       },
+    //       {
+    //         title: 'صندوق حمایت و بازنشستگی  کارکنان فولاد ',
+    //         children: [
+    //           { title: '5سرمایه گذاری آتیه صبا' },
+    //           { title: '5سرمایه گذاری  صندوقبازنشستگی کشوری' },
+    //           { title: '5گروه مدیریت ارزش سرمایه' }
+    //         ]
+    //       },
+    //     ]
+    //   }
+    // ]
 
+  }
+
+  getAllCompanyList() {
+    this.httpService
+      .get<CompanyTree[]>(CompanyTree.apiAddress)
+      .subscribe(response => {
+        if (response.data.result) {
+          this.companyList = response.data.result;
+        }
+      });
   }
 
   changeStyleOfSideBar() {
