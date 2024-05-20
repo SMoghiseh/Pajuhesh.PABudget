c8394d51fa407d5ed1f2f607ba002ebbe1511515
m.vahid <m.vahid@pajuheshco.com>
2024-05-20 13:47:21 +0330

    add co profile

---
+++ src/app/modules/comapny-management/company-profile/company-profile.component.ts
@@ -0,0 +1,147 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '@core/http/http.service';
+import { Company, Dashboard, UrlBuilder } from '@shared/models/response.model';
+import { map } from 'rxjs';
+import Chart from 'chart.js/auto';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-company-profile',
+  templateUrl: './company-profile.component.html',
+  styleUrls: ['./company-profile.component.scss'],
+})
+export class CompanyProfileComponent implements OnInit {
+  infoLst = new Company();
+  myChart!: Chart;
+  benefitCost: any;
+
+  constructor(
+    private httpService: HttpService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const coId = params['id'];
+      this.getProfileCoInfo(coId);
+      this.getCashBudgetByMonth(coId);
+      this.getShareholdersDashboard(coId);
+      this.getCostAndBenefitForProfile(coId);
+    });
+  }
+
+  getProfileCoInfo(id: string) {
+    this.httpService
+      .get<Company>(
+        UrlBuilder.build(
+          Company.apiAddressDetailCo + 'profileCompany/' + id,
+          ''
+        )
+      )
+      .pipe(
+        map(response => {
+          if (response.data && response.data.result) {
+            return response.data.result;
+          } else return new Company();
+        })
+      )
+      .subscribe(info => {
+        this.infoLst = info;
+      });
+  }
+
+  createBarChart(labels: Array<string>, ds: Array<any>, chartName: string) {
+    new Chart(chartName, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: ds,
+      },
+      options: {
+        plugins: {
+          legend: {
+            labels: {
+              font: {
+                family: 'shabnam',
+              },
+            },
+            display: chartName === 'BarChart2' ? false : true,
+          },
+          tooltip: {
+            titleFont: {
+              family: 'shabnam',
+            },
+            bodyFont: {
+              family: 'shabnam',
+            },
+          },
+        },
+
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
+
+  getCashBudgetByMonth(id: string) {
+    this.httpService
+      .get<Dashboard>(
+        UrlBuilder.build(
+          Dashboard.apiAddressCashBudgetByMonthForProfile + id,
+          ''
+        )
+      )
+      .pipe(
+        map(response => {
+          if (response.data && response.data.result) {
+            return response.data.result;
+          } else return new Dashboard();
+        })
+      )
+      .subscribe(coInfo => {
+        const labels = coInfo.labels;
+        const counts = coInfo.datasets;
+        this.createBarChart(labels, counts, 'BarChart');
+      });
+  }
+
+  getShareholdersDashboard(id: string) {
+    this.httpService
+      .get<Dashboard>(
+        UrlBuilder.build(Dashboard.apiAddressShareholdersDashboard + id, '')
+      )
+      .pipe(
+        map(response => {
+          if (response.data && response.data.result) {
+            return response.data.result;
+          } else return new Dashboard();
+        })
+      )
+      .subscribe(coInfo => {
+        const labels = coInfo.labels;
+        const counts = coInfo.datasets;
+        this.createBarChart(labels, counts, 'BarChart2');
+      });
+  }
+
+  getCostAndBenefitForProfile(id: string) {
+    this.httpService
+      .get<number>(
+        UrlBuilder.build(Dashboard.apiAddressCostAndBenefitForProfile + id, '')
+      )
+      .pipe(
+        map(response => {
+          if (response.data) {
+            return response.data;
+          } else return 0;
+        })
+      )
+      .subscribe(res => {
+        this.benefitCost = res;
+      });
+  }
+}
