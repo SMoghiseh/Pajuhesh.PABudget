c587233f487c3e949b2fa70c48b3e9baf02f65f8
M.Jafari <M.Jafari@pajuheshco.com>
2024-07-06 01:00:00 -0700

    fix: fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,112 +9,195 @@ import { PageNotFoundComponent } from './core/layout/page-not-found/page-not-fou
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
+
   {
+
     path: '',
-    component: AppComponent,
+
+    component: MainComponent,
+
     title: 'برنامه بودجه',
+
     canActivate: [authGuard],
-    children:[
+
+    data: { animation: 'mainPage' },
+
+    children: [
+
       {
-        path: '',
-        component: MainComponent,
-        title: 'برنامه بودجه',
-        canActivate: [authGuard],
-        data: { animation: 'mainPage' },
-        children: [
-          {
-            path: 'default',
-            title: 'برنامه بودجه | صفحه اصلی',
-            loadChildren: () =>
-              import('../app/modules/home/home.module').then(
-                module => module.HomeModule
-              ),
-            canMatch: [authGuard],
-          },
-          {
-            path: 'Operation',
-            title: 'برنامه و بودجه | عملیات',
-            loadChildren: () =>
-              import('./modules/operation/operation.module').then(
-                m => m.OperationModule
-              ),
-            canMatch: [authGuard],
-          },
-          {
-            path: 'baseinfo',
-            title: 'برنامه و بودجه | مدیریت اطلاعات پایه',
-            loadChildren: () =>
-              import('./modules/basics-management/basics-management.module').then(
-                m => m.BasicsManagementModule
-              ),
-            canMatch: [authGuard],
-          },
-          {
-            path: 'UserManagment',
-            title: 'برنامه بودجه | مدیریت کاربران',
-            loadChildren: () =>
-              import('./modules/user-management/user-management.module').then(
-                m => m.UserManagementModule
-              ),
-            canMatch: [authGuard],
-          },
-          {
-            path: 'DocSettings',
-            title: 'برنامه و بودجه | تنظیمات پایه اسناد',
-            loadChildren: () =>
-              import('./modules/document-settings/document-settings.module').then(
-                m => m.DocumentSettingsModule
-              ),
-            canMatch: [authGuard],
-          },
-          {
-            path: 'Comapny',
-            title: 'برنامه و بودجه |مدیریت سازمان ها',
-            loadChildren: () =>
-              import('./modules/comapny-management/comapny-management.module').then(
-                m => m.ComapnyManagementModule
-              ),
-            canMatch: [authGuard],
-          },
-          {
-            path: 'FinancialexpertDocuments',
-            title: 'برنامه و بودجه | مدیریت تاییدات',
-            loadChildren: () =>
-              import('./modules/financial-expert/financial-expert.module').then(
-                m => m.FinancialExpertModule
-              ),
-            canMatch: [authGuard],
-          },
-        ],
-      },
-      {
-        path: 'TreeOrganization',
-        title: 'برنامه و بودجه |  ساختار درختی شرکت ها ',
+
+        path: 'default',
+
+        title: 'برنامه بودجه | صفحه اصلی',
+
         loadChildren: () =>
-          import('./modules/tree-organization/tree-organization.module').then(
-            m => m.TreeOrganizationModule
+
+          import('../app/modules/home/home.module').then(
+
+            module => module.HomeModule
+
           ),
+
+        canMatch: [authGuard],
+
       },
+
       {
-        path: 'AllCompanyReports',
-        title: 'برنامه و بودجه |  اطلاعات شرکت ها ',
+
+        path: 'Operation',
+
+        title: 'برنامه و بودجه | عملیات',
+
         loadChildren: () =>
-          import('./modules/all-company-reports/all-company-reports.module').then(
-            m => m.AllCompanyReportsModule
-          )
+
+          import('./modules/operation/operation.module').then(
+
+            m => m.OperationModule
+
+          ),
+
+        canMatch: [authGuard],
+
       },
+
       {
-        path: 'account',
-        title: 'برنامه بودجه | ورود',
-        loadChildren: () => AccountModule,
+
+        path: 'baseinfo',
+
+        title: 'برنامه و بودجه | مدیریت اطلاعات پایه',
+
+        loadChildren: () =>
+
+          import('./modules/basics-management/basics-management.module').then(
+
+            m => m.BasicsManagementModule
+
+          ),
+
+        canMatch: [authGuard],
+
       },
-    ]
-  
+
+      {
+
+        path: 'UserManagment',
+
+        title: 'برنامه بودجه | مدیریت کاربران',
+
+        loadChildren: () =>
+
+          import('./modules/user-management/user-management.module').then(
+
+            m => m.UserManagementModule
+
+          ),
+
+        canMatch: [authGuard],
+
+      },
+
+      {
+
+        path: 'DocSettings',
+
+        title: 'برنامه و بودجه | تنظیمات پایه اسناد',
+
+        loadChildren: () =>
+
+          import('./modules/document-settings/document-settings.module').then(
+
+            m => m.DocumentSettingsModule
+
+          ),
+
+        canMatch: [authGuard],
+
+      },
+
+      {
+
+        path: 'Comapny',
+
+        title: 'برنامه و بودجه |مدیریت سازمان ها',
+
+        loadChildren: () =>
+
+          import('./modules/comapny-management/comapny-management.module').then(
+
+            m => m.ComapnyManagementModule
+
+          ),
+
+        canMatch: [authGuard],
+
+      },
+
+      {
+
+        path: 'FinancialexpertDocuments',
+
+        title: 'برنامه و بودجه | مدیریت تاییدات',
+
+        loadChildren: () =>
+
+          import('./modules/financial-expert/financial-expert.module').then(
+
+            m => m.FinancialExpertModule
+
+          ),
+
+        canMatch: [authGuard],
+
+      },
+
+    ],
+
   },
 
-  
+  {
+
+    path: 'TreeOrganization',
+
+    title: 'برنامه و بودجه | ساختار درختی شرکت ها ',
+
+    loadChildren: () =>
+
+      import('./modules/tree-organization/tree-organization.module').then(
+
+        m => m.TreeOrganizationModule
+
+      ),
+
+  },
+
+  {
+
+    path: 'AllCompanyReports',
+
+    title: 'برنامه و بودجه | اطلاعات شرکت ها ',
+
+    loadChildren: () =>
+
+      import('./modules/all-company-reports/all-company-reports.module').then(
+
+        m => m.AllCompanyReportsModule
+
+      )
+
+  },
+
+  {
+
+    path: 'account',
+
+    title: 'برنامه بودجه | ورود',
+
+    loadChildren: () => AccountModule,
+
+  },
 
   { path: '**', component: PageNotFoundComponent },
+
 ];
 
 @NgModule({
@@ -123,4 +206,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
