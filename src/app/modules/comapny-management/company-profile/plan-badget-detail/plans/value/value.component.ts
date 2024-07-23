d0ac3536b255181b04cad083aa9e50b5f515fdd3
M.Jafari <M.Jafari@pajuheshco.com>
2024-07-23 03:26:01 -0700

    change field

--- src/app/modules/comapny-management/company-profile/plan-badget-detail/plans/value/value.component.ts
+++ src/app/modules/comapny-management/company-profile/plan-badget-detail/plans/value/value.component.ts
@@ -20,7 +20,7 @@ export class ValueComponent {
   getPlanDetail(yearId: number) {
     const body = {
       companyId: this.inputData.companyId,
-      staticYearId: yearId,
+      periodId: yearId,
     };
     this.httpService
       .post<any>(UrlBuilder.build(Plan.apiAddressValue, ''), body)
