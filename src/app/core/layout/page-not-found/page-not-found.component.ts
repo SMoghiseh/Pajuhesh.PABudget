45e394d65b5e8521e33f825d74c2e06d212eebb7
s.moqiseh <s.moqiseh@pajuheshco.com>
2024-07-24 13:51:03 +0330

    fixed: page Found back button

--- src/app/core/layout/page-not-found/page-not-found.component.ts
+++ src/app/core/layout/page-not-found/page-not-found.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent {
+  constructor(private _location: Location) {}
+  backClicked() {
+    this._location.back();
+  }
+}
